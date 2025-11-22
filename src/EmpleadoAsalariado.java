818ec762e06eeb3ea1470839546d4222348afac4
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 12:59:42 +0100

    Update EmpleadoAsalariado.java
    Introduccion de comentarios

--- src/EmpleadoAsalariado.java
+++ src/EmpleadoAsalariado.java
@@ -1,11 +1,12 @@
+//Creamos la clase con la union de la clase abstracta
 public class EmpleadoAsalariado extends Empleado {
     private int mes;
-
+//Creamos el constructor 
     public EmpleadoAsalariado(double salario,int mes) {
         super(salario);
         this.mes = mes;
     }
-
+//Creamos el metodo calcularsalario
     @Override
     public double calcularSalario() {
         return salario*mes;
