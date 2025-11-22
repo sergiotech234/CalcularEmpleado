06506809c71f4b01242cdb30596604c55ddfb483
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 14:31:22 +0100

    Update EmpleadoPorHoras.java
    Introduccion de comentarios

--- src/EmpleadoPorHoras.java
+++ src/EmpleadoPorHoras.java
@@ -1,9 +1,12 @@
+//Creamos la calse EmpleadoPorHoras y lo anexamos con la clase abastarcta Empleado
 public class EmpleadoPorHoras extends Empleado {
     private double horas;
+    //Creamos el constructor 
     public EmpleadoPorHoras(double salario,double horas) {
         super(salario);
         this.horas = horas;
     }
+    //Creamos el metodo calcular salario
     @Override
     public double calcularSalario() {
         return salario * horas;
