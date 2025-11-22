b43d7929155ab8e81b6624e58d55b4ffe1175673
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 12:56:33 +0100

    Update Empleado.java
    Introduccimos comentarios

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,12 +1,13 @@
+//Creamos la clase abstracta empleado
 public abstract class Empleado {
     protected double salario;
-
+//Creamos un constructor con la variable creada 
     public Empleado(double salario) {
         this.salario = salario;
     }
-
+//creamos el metodo de calcular salaraio
     public abstract double calcularSalario();
-
+//Creamos el metodo de mostararInformacion
     public void mostrarInformacion() {;
         System.out.println("Salario: $" + calcularSalario());
 
